93ed127872e69490f2dc03e4482f4854f444ea00
Black-tag <unnianandunni007@gmail.com>
2025-03-15 07:31:42 +0000

    player side is completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 
 
@@ -8,13 +10,29 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+    Player.containers = (updatable,drawable)
     while (1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)        
         screen.fill(0, rect=None, special_flags=0)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-
+        # clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
